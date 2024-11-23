4d4c9cb0f13d4f11bbf6fc8aa462bdb132229725
thuong022101045@tgu.edu.vn <thuong022101045@tgu.edu.vn>
2024-11-23 08:03:32 +0700

    add button and datagridview

--- Form1/Form1/Form1.cs
+++ Form1/Form1/Form1.cs
@@ -16,5 +16,10 @@ namespace Form1
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
